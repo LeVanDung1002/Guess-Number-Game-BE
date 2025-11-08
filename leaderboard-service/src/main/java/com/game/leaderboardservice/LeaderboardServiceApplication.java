7910059e65895c379df9b23cf822cdb0b23a8cb5
Lê Văn Dũng <ledung10022001@gmail.com>
2025-11-08 22:56:07 +0700

    Add microservice base

---
+++ leaderboard-service/src/main/java/com/game/leaderboardservice/LeaderboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.game.leaderboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardServiceApplication.class, args);
+	}
+
+}
